8531dfa98a7cc80423b5decd1b58813da262ef7f
Melvin Kempe <k3mp314@gmail.com>
2025-05-15 14:30:59 +0200

    Initialize repo

---
+++ models/Booking.mjs
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Kundens förnamn måste anges"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Kundens efternamn måste anges"],
+  },
+  phone: {
+    type: String,
+    required: [true, "Kundens telefonnummer måste anges"],
+  },
+  email: {
+    type: String,
+    required: [true, "Kundens e-post adress måste anges"],
+  },
+  image: {
+    type: File,
+    required: false,
+  },
+});
+
+export default mongoose.model("Booking", bookingSchema);
