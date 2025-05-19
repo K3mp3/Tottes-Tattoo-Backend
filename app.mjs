c26aa24bf162329042596a6dc64b0bbca3599e4e
Bagher hosseini <Bagher.Hosseini@yh.nackademin.se>
2025-05-19 11:55:17 +0200

    Add static file serving for images directory

--- app.mjs
+++ app.mjs
@@ -35,7 +35,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use("/api/v1", bookingRouter);
 
